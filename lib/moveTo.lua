1c3d83764a8492a7a467f2aad616323e8c8c82da
Brad <the.naragath@gmail.com>
2017-09-29 01:44:21 -0600

    Started on the moving library

--- lib/moveTo.lua
+++ lib/moveTo.lua
@@ -4,9 +4,23 @@
 
 --You may modify this file as long as you don't claim this to be your own work and give me credit.
 
-local robot = require("robot");
-local comp = require("component");
-local nav = comp.navigation;
+--local robot = require("robot");
+--local comp = require("component");
+--local nav = comp.navigation;
+
+local function proxyFor(name, required)
+  local address = component and component.list(name)()
+  if not address and required then
+    error("missing component '" .. name .. "'")
+  end
+  return address and component.proxy(address) or nil
+end
+
+local robot = proxyFor("robot",true);
+local nav = proxyFor("nvigation",true);
+
+
+
 local zeroNav = {
 					firstRun = true,
 					data = {}
