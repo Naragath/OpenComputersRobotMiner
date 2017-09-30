485b1bbb60d6ade054c03b8ad733400c2b139e98
Brad <the.naragath@gmail.com>
2017-09-30 15:15:20 -0600

    Day three of creating the move library

--- lib/moveTo.lua
+++ lib/moveTo.lua
@@ -22,8 +22,11 @@
 
 ]]
 
+
+
 local robot = require("robot");
 local component = require("component");
+local sides = require("sides");
 local nav = component.navigation;
 
 local moveTo = {
@@ -74,7 +77,6 @@ function moveTo.noNav()  --Should only call if there is no navigation component,
 end
 
 function moveTo.setHome(navID)  --Make sure to call this before returnHome so that it knows where "home" is at(used to calculate where each chunk available for mining is at at).
-	if not type(nav) == nil then
 		moveTo.updateWaypoints();
 		moveTo.homePoint = fWP(function(w) return w.label == navID; end);
 		if next(moveTo.homePoint) == nil then
@@ -82,34 +84,33 @@ function moveTo.setHome(navID)  --Make sure to call this before returnHome so th
 		else
 			moveTo.hasHome = true;
 		end
-	else
-		moveTo.noNav();
-	end
 end
 
 function moveTo.returnHome()  --Useful to get the robot to go to a spot that the player can easily get to(assuming they don't have or want to use flight)
-		if not type(nav) == nil then
 			if not moveTo.hasHome then
 				print("Home waypoint has not been set, did it get removed?");
 			else
 				print("Doing some nav stuffz.");
 				moveTo.moveTo(moveTo.homePoint.position);
 			end
-		else
-			moveTo.noNav();
-		end
 end
 
 function moveTo.moveTick()  --Without nav we'll have to assume that the first way it's set down is north(or south?), perhaps expose a way for the user to set it in the browser setup?
-	if not type(nav) == nil then
 		if moveTo.tPosition.x - moveTo.position.x > 0 then
 			moveTo.currentFacing = nav.getFacing();
+			if moveTo.currentFacing == sides.east then
+				local success, why = robot.forward();
+				if success == nil then
+					print(why);
+				end
+			elseif moveTo.currentFacing == sides.north or sides.west then
+				robot.turnRight();
+			else
+				robot.turnLeft();
+			end
 		else
 			
 		end
-	else
-		moveTo.noNav();
-	end
 end
 
 function moveTo.moveTo(x,y,z)  --Sets where the robot is going to move to, DOES NOT DO ANY MOVEMENT, that is handled in moveTick().
--- lib/version.lua
+++ lib/version.lua
@@ -1,7 +1,7 @@
 local version = {
   major = 0,
   minor = 0,
-  build = 43
+  build = 44
 }
 
 return version;
