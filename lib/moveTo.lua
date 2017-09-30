3f1dbf7009f925f8f2496dd01014fe53aff67b1e
Brad <the.naragath@gmail.com>
2017-09-30 15:06:00 -0600

    Day three of creating the move library

--- lib/moveTo.lua
+++ lib/moveTo.lua
@@ -74,7 +74,7 @@ function moveTo.noNav()  --Should only call if there is no navigation component,
 end
 
 function moveTo.setHome(navID)  --Make sure to call this before returnHome so that it knows where "home" is at(used to calculate where each chunk available for mining is at at).
-	if not nav == nil then
+	if not type(nav) == nil then
 		moveTo.updateWaypoints();
 		moveTo.homePoint = fWP(function(w) return w.label == navID; end);
 		if next(moveTo.homePoint) == nil then
@@ -88,7 +88,7 @@ function moveTo.setHome(navID)  --Make sure to call this before returnHome so th
 end
 
 function moveTo.returnHome()  --Useful to get the robot to go to a spot that the player can easily get to(assuming they don't have or want to use flight)
-		if not nav == nil then
+		if not type(nav) == nil then
 			if not moveTo.hasHome then
 				print("Home waypoint has not been set, did it get removed?");
 			else
@@ -101,7 +101,7 @@ function moveTo.returnHome()  --Useful to get the robot to go to a spot that the
 end
 
 function moveTo.moveTick()  --Without nav we'll have to assume that the first way it's set down is north(or south?), perhaps expose a way for the user to set it in the browser setup?
-	if not nav == nil then
+	if not type(nav) == nil then
 		if moveTo.tPosition.x - moveTo.position.x > 0 then
 			moveTo.currentFacing = nav.getFacing();
 		else
